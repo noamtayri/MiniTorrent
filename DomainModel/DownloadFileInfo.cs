b4c4654cfeaeea67d2da129208d65425f3d9ed37
Noam <noamtayri1990@gmail.com>
2018-02-26 13:25:07 +0200

    add DownloadFileInfo to DomainModel
    add UploadFileInfo to DomainModel

---
+++ DomainModel/DownloadFileInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class DownloadFileInfo
+    {
+        public string FileName { get; set; }
+        public long FileSize { get; set; }
+        //public int fileSize { get; set; }
+        public string Status { get; set; }
+        public string Time { get; set; }
+        public int Kbps { get; set; }
+
+        public DownloadFileInfo(string fileName, long fileSize, string status, string time, int kbps)
+        {
+            FileName = fileName;
+            FileSize = fileSize;
+            Status = status;
+            Time = time;
+            Kbps = kbps;
+        }
+    }
+}
---
+++ DomainModel/UploadFileInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class UploadFileInfo
+    {
+        public string FileName { get; set; }
+        public long FileSize { get; set; } 
+        //public int FileSize { get; set; }
+        public string Status { get; set; }
+
+        public UploadFileInfo(string fileName, long fileSize, string status)
+        {
+            FileName = fileName;
+            FileSize = fileSize;
+            Status = status;
+        }
+    }
+}
